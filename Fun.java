0181e56eaa1add03968f4a4186be0ace413463de
SanketS1997 <101618340+SanketS1997@users.noreply.github.com>
2022-03-15 12:09:03 +0530

    Add files via upload

---
+++ Fun.java
@@ -0,0 +1,25 @@
+class fun
+
+{
+
+    static void add(int n1, int n2)//defn
+
+    {
+
+        int sum=n1+n2;
+
+        System.out.println("sum is==>"+sum);
+
+    }
+
+    public static void main(String[] args)
+
+    {
+
+        //System.out.println(add(45,5));//call
+
+        add(4,5);
+
+    }
+
+}
